316ed3e98169a4a043984b1a6bcdfa5dc223d0cd
Oetske <oetske.lrx@gmail.com>
2021-04-15 15:56:10 +0200

    Design HomePage Fait Background avec tous les différents films ça ne va pas. L'oeil se perd dans de l'information inutile, l'essentiel du site est perdu. Soit mettre une texture en fond (en attendant exemple avec rideaux) Soit faire le design 1.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule} from '@angular/material/card';
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {HomeComponent} from './home';
-import {LobbyComponent} from './lobby';
-import {MatchmakingComponent} from './matchmaking';
+import { HomeComponent } from './home';
+import { LobbyComponent } from './lobby';
+import { MatchmakingComponent } from './matchmaking';
+
+
 
 @NgModule({
   declarations: [
@@ -17,8 +23,12 @@ import {MatchmakingComponent} from './matchmaking';
   ],
   imports: [
     BrowserModule,
+    MatSliderModule,
+    MatButtonModule,
+    MatCardModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    MatToolbarModule,
   ],
   providers: [],
   bootstrap: [AppComponent]

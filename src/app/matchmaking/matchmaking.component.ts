b1e465a557d0468ab97b936477576de11b77e9a1
Bayarbileg Batbileg <bayarbileg.batbileg@etu.unige.ch>
2021-06-09 19:49:16 +0000

    New site

--- src/app/matchmaking/matchmaking.component.ts
+++ src/app/matchmaking/matchmaking.component.ts
@@ -1,62 +1,81 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Movie } from '../models/Movie.model';
+import { Subscription } from 'rxjs';
+import { MovieService } from '../services/movie.service';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 
-@Component({ templateUrl: 'matchmaking.component.html' })
-export class MatchmakingComponent {
-  public index = 0;
+@Component({templateUrl: 'matchmaking.component.html'})
 
-  constructor() { }
+export class MatchmakingComponent implements OnInit, OnDestroy {
+  score = 0;
+  movies: Movie[];
+  movieSubscription: Subscription;
+  index = 0;
+  interval: number;
 
-    public genres = [
-      {id: 0, name: 'comedy',
-        img: 'https://m.media-amazon.com/images/G/01/IMDb/genres/Comedy._CB1513316167_SX233_CR0,0,233,131_AL_.jpg'},
-      {id: 1, name: 'sci-fi',
-        img: 'https://m.media-amazon.com/images/G/01/IMDb/genres/Sci-Fi._CB1513316168_SX233_CR0,0,233,131_AL_.jpg'},
-      {id: 2, name: 'horror',
-        img: 'https://m.media-amazon.com/images/G/01/IMDb/genres/Horror._CB1513316168_SX233_CR0,0,233,131_AL_.jpg'},
-      {id: 3, name: 'romance',
-      img: 'https://m.media-amazon.com/images/G/01/IMDb/genres/Romance._CB1513316168_SX233_CR0,0,233,131_AL_.jpg'},
-      {id: 4, name: 'action',
-      img: 'https://m.media-amazon.com/images/G/01/IMDb/genres/Action._CB1513316166_SX233_CR0,0,233,131_AL_.jpg'},
-      {id: 5, name: 'thriller' },
-      {id: 6, name: 'drama' },
-      {id: 7, name: 'mystery' },
-      {id: 8, name: 'crime' },
-      {id: 9, name: 'animation' },
-      {id: 10, name: 'adventure' },
-      {id: 11, name: 'fantasy' },
-      {id: 12, name: 'comedy-romance' },
-      {id: 13, name: 'action-comedy' },
-      {id: 14, name: 'superhero' }
-    ];
+  // URL pour play service
+  scoreURL = 'https://movie.graved.ch/api/play/v1/play/send';
+  finishURL = 'https://movie.graved.ch/api/play/v1/play/checkAllFinished';
+  // URL pour lobby service
+  endGameURL = 'https://movie.graved.ch/api/lobby/v1/lobby/endGameDeletion';
 
+  constructor(private movieService: MovieService,
+              private http: HttpClient,
+              private router: Router) { }
 
-     public films = [
-      {
-        name: 'the office',
-        genre: 'comedy',
-        year: '2005–2013',
-        storyline: 'A mockumentary on a group of typical office workers, where the workday consists of ego clashes, inappropriate behavior, and tedium. ',
-        creators: 'Greg Daniels, Ricky Gervais, Stephen Merchant',
-        stars: ' Steve Carell, Jenna Fischer, John Krasinski',
-        img: 'https://m.media-amazon.com/images/M/MV5BMDNkOTE4NDQtMTNmYi00MWE0LWE4ZTktYTc0NzhhNWIzNzJiXkEyXkFqcGdeQXVyMzQ2MDI5NjU@._V1_UX182_CR0,0,182,268_AL_.jpg'
-      },
-      {
-        name: 'Mortal Kombat',
-        genre: 'action',
-        year: '2021',
-        storyline: 'MMA fighter Cole Young seeks out Earth\'s greatest champions in order to stand against the enemies of Outworld in a high stakes battle for the universe. ',
-        creators: 'Greg Russo, Dave Callaham',
-        stars: 'Lewis Tan, Jessica McNamee, Josh Lawson',
-        img: 'https://m.media-amazon.com/images/M/MV5BY2ZlNWIxODMtN2YwZi00ZjNmLWIyN2UtZTFkYmZkNDQyNTAyXkEyXkFqcGdeQXVyODkzNTgxMDg@._V1_UX182_CR0,0,182,268_AL_.jpg',
-      }
-    ];
-     formatLabel(value: number): any {
+  formatLabel(value: number): any {
     if (value >= 1000) {
       return Math.round(value / 1000);
     }
-
     return value;
   }
+
+  ngOnInit(): void {
+    this.movieSubscription = this.movieService.moviesSubject.subscribe(
+      (movies: Movie[]) => {
+        this.movies = movies;
+      }
+    );
+    this.movieService.emitMovies();
+    this.interval = setInterval(this.checkAllFinished, 5000);
+  }
+
+  ngOnDestroy(): void {
+    this.movieSubscription.unsubscribe();
+    clearInterval(this.interval);
+  }
+
+  sendScore(index, score): void {
+    this.http.post(`${this.scoreURL}/${index}/${score}`, null).subscribe(
+      (result) => {
+        console.log(result);
+      });
+    this.index++;
+  }
+
+  checkAllFinished(): void {
+    this.http.get(this.finishURL)
+      .subscribe(
+        (result) => {
+          console.log(result);
+        });
+  }
+
+  getScore( event ): void {
+    this.score = event.value;
+  }
+
+  endGame(): void {
+    this.http.delete(this.endGameURL)
+      .subscribe(
+        (result) => {
+          console.log(result);
+        });
+  }
+
 }
+
+
 
 
---
+++ src/app/models/Movie.model.ts
@@ -0,0 +1,9 @@
+export class Movie {
+  constructor(
+    public title: string,
+    public overview: string,
+    public voteAverage: number,
+    public releaseDates: string,
+    public image: string,
+  ) {}
+}
